8eebc22f2cf37a185dcbddc632d80a58c2840869
edono morrison <edonomorrison@gmail.com>
2019-07-04 15:12:18 +0100

    added app package

---
+++ app/config/config.go
@@ -0,0 +1,201 @@
+// Copyright (c) 2018-2019 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package config
+
+import (
+	"fmt"
+	"os"
+	
+	flags "github.com/jessevdk/go-flags"
+)
+
+const (
+	DefaultConfigFilename = "dcrextdata.conf"
+	defaultLogFilename    = "dcrextdata.log"
+	defaultLogLevel       = "info"
+	hint                  = `Run dcrextdata --mode=http to start http server or just dcrextdata`
+)
+
+type Config struct {
+	ConfigFileOptions
+	CommandLineOptions
+}
+
+type ConfigFileOptions struct {
+	// General application behaviour
+	Reset      bool   `short:"R" long:"reset" description:"Drop all database tables and start over"`
+	LogFile    string `short:"L" long:"logfile" description:"File name of the log file"`
+	ConfigFile string `short:"C" long:"Configfile" description:"Path to Configuration file"`
+	DebugLevel string `short:"d" long:"debuglevel" description:"Logging level {trace, debug, info, warn, error, critical}"`
+	Quiet      bool   `short:"q" long:"quiet" description:"Easy way to set debuglevel to error"`
+
+	// Postgresql Configuration
+	DBHost string `long:"dbhost" description:"Database host"`
+	DBPort string `long:"dbport" description:"Database port"`
+	DBUser string `long:"dbuser" description:"Database username"`
+	DBPass string `long:"dbpass" description:"Database password"`
+	DBName string `long:"dbname" description:"Database name"`
+
+	// Http Server
+	HTTPHost string `long:"httphost" description:"HTTP server host address or IP when running godcr in http mode."`
+	HTTPPort string `long:"httpport" description:"HTTP server port when running godcr in http mode."`
+	// Exchange collector
+	DisableExchangeTicks bool     `long:"disablexcticks" decription:"Disables collection of ticker data from exchanges"`
+	DisabledExchanges    []string `long:"disableexchange" description:"Disable data collection for this exchange"`
+
+	// PoW collector
+	DisablePow   bool     `long:"disablepow" description:"Disables collection of data for pows"`
+	DisabledPows []string `long:"disabledpow" description:"Disable data collection for this Pow"`
+	PowInterval  int64    `long:"powI" description:"Collection interval for Pow"`
+
+	// VSP
+	DisableVSP  bool  `long:"disablevsp" description:"Disables periodic voting service pool status collection"`
+	VSPInterval int64 `long:"vspinterval" description:"Collection interval for pool status collection"`
+
+	// Mempool
+	DisableMempool  bool   `long:"disablemempool" description:"Disable mempool data collection"`
+	DcrdRpcServer   string `long:"dcrdrpcserver"`
+	DcrdNetworkType string `long:"dcrdnetworktype"`
+	DcrdRpcUser     string `long:"dcrdrpcuser"`
+	DcrdRpcPassword string `long:"dcrdrpcpaswword"`
+}
+
+// CommandLineOptions holds the top-level options/flags that are displayed on the command-line menu
+type CommandLineOptions struct {
+	HttpMode bool `long:"http" description:"Launch http server"`
+}
+
+func defaultFileOptions() ConfigFileOptions {
+	return ConfigFileOptions{
+		LogFile:         defaultLogFilename,
+		ConfigFile:      DefaultConfigFilename,
+		DebugLevel:      defaultLogLevel,
+		VSPInterval:     300,
+		PowInterval:     300,
+		DcrdNetworkType: "mainnet",
+	}
+}
+
+func defaultConfig() Config {
+	return Config{
+		ConfigFileOptions: defaultFileOptions(),
+	}
+}
+
+// // validLogLevel returns whether or not logLevel is a valid debug log level.
+// func validLogLevel(logLevel string) bool {
+// 	_, ok := slog.LevelFromString(logLevel)
+// 	return ok
+// }
+
+// // supportedSubsystems returns a sorted slice of the supported subsystems for
+// // logging purposes.
+// func supportedSubsystems() []string {
+// 	// Convert the subsystemLoggers map keys to a slice.
+// 	subsystems := make([]string, 0, len(subsystemLoggers))
+// 	for subsysID := range subsystemLoggers {
+// 		subsystems = append(subsystems, subsysID)
+// 	}
+
+// 	// Sort the subsytems for stable display.
+// 	sort.Strings(subsystems)
+// 	return subsystems
+// }
+
+// // parseAndSetDebugLevels attempts to parse the specified debug level and set
+// // the levels accordingly.  An appropriate error is returned if anything is
+// // invalid.
+// func parseAndSetDebugLevels(debugLevel string) error {
+// 	// When the specified string doesn't have any delimters, treat it as
+// 	// the log level for all subsystems.
+// 	if !strings.Contains(debugLevel, ",") && !strings.Contains(debugLevel, "=") {
+// 		// Validate debug log level.
+// 		if !validLogLevel(debugLevel) {
+// 			str := "The specified debug level [%v] is invalid"
+// 			return fmt.Errorf(str, debugLevel)
+// 		}
+
+// 		// Change the logging level for all subsystems.
+// 		setLogLevels(debugLevel)
+
+// 		return nil
+// 	}
+
+// 	// Split the specified string into subsystem/level pairs while detecting
+// 	// issues and update the log levels accordingly.
+// 	for _, logLevelPair := range strings.Split(debugLevel, ",") {
+// 		if !strings.Contains(logLevelPair, "=") {
+// 			str := "The specified debug level contains an invalid " +
+// 				"subsystem/level pair [%v]"
+// 			return fmt.Errorf(str, logLevelPair)
+// 		}
+
+// 		// Extract the specified subsystem and log level.
+// 		fields := strings.Split(logLevelPair, "=")
+// 		subsysID, logLevel := fields[0], fields[1]
+
+// 		// Validate subsystem.
+// 		if _, exists := subsystemLoggers[subsysID]; !exists {
+// 			str := "The specified subsystem [%v] is invalid -- " +
+// 				"supported subsytems %v"
+// 			return fmt.Errorf(str, subsysID, supportedSubsystems())
+// 		}
+
+// 		// Validate log level.
+// 		if !validLogLevel(logLevel) {
+// 			str := "The specified debug level [%v] is invalid"
+// 			return fmt.Errorf(str, logLevel)
+// 		}
+
+// 		setLogLevel(subsysID, logLevel)
+// 	}
+
+// 	return nil
+// }
+
+func LoadConfig() (*Config, []string, error) {
+	cfg := defaultConfig()
+	parser := flags.NewParser(&cfg, flags.IgnoreUnknown)
+	err := flags.NewIniParser(parser).ParseFile(cfg.ConfigFileOptions.ConfigFile)
+	if err != nil {
+		if _, ok := err.(*os.PathError); ok {
+			fmt.Printf("Missing Config file %s in current directory\n", cfg.ConfigFileOptions.ConfigFile)
+		} else {
+			return nil, nil, err
+		}
+	}
+
+	unknownArg, err := parser.Parse()
+	if err != nil {
+		e, ok := err.(*flags.Error)
+		if ok && e.Type == flags.ErrHelp {
+			os.Exit(0)
+		}
+		return nil, nil, err
+	}
+
+	// initLogRotator(cfg.ConfigFileOptions.LogFile)
+
+	// // Special show command to list supported subsystems and exit.
+	// if cfg.ConfigFileOptions.DebugLevel == "show" {
+	// 	fmt.Println("Supported subsystems", supportedSubsystems())
+	// 	os.Exit(0)
+	// }
+
+	// // Parse, validate, and set debug log level(s).
+	// if cfg.Quiet {
+	// 	cfg.ConfigFileOptions.DebugLevel = "error"
+	// }
+
+	// // Parse, validate, and set debug log level(s).
+	// if err := parseAndSetDebugLevels(cfg.ConfigFileOptions.DebugLevel); err != nil {
+	// 	err = fmt.Errorf("%s: %v", "loadConfig", err.Error())
+	// 	fmt.Fprintln(os.Stderr, err)
+	// 	parser.WriteHelp(os.Stderr)
+	// 	return nil, nil, err
+	// }
+
+	return &cfg, unknownArg, nil
+}
--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/jessevdk/go-flags"
 	"github.com/decred/dcrd/chaincfg"
 	"github.com/decred/dcrd/dcrutil"
 	"github.com/decred/dcrd/rpcclient"
@@ -22,9 +23,11 @@ import (
 	"github.com/raedahgroup/dcrextdata/mempool"
 	"github.com/raedahgroup/dcrextdata/postgres"
 	"github.com/raedahgroup/dcrextdata/pow"
-	"github.com/raedahgroup/dcrextdata/version"
 	"github.com/raedahgroup/dcrextdata/vsp"
 	"github.com/raedahgroup/dcrextdata/web"
+	"github.com/raedahgroup/dcrextdata/app"
+	"github.com/raedahgroup/dcrextdata/app/config"
+	"github.com/raedahgroup/dcrextdata/app/help"
 )
 
 // const dcrlaunchtime int64 = 1454889600
@@ -50,20 +53,58 @@ func main() {
 }
 
 func _main(ctx context.Context) error {
-	cfg, _, err := loadConfig()
+	cfg, args, err := config.LoadConfig()
 	if err != nil {
 		return err
 	}
+	fmt.Println(args)
 
+	// Initialize log rotation.  After log rotation has been initialized, the
+	// logger variables may be used.
+	initLogRotator(cfg.ConfigFileOptions.LogFile)
 	defer func() {
 		if logRotator != nil {
 			logRotator.Close()
 		}
 	}()
 
+	// Special show command to list supported subsystems and exit.
+	if cfg.DebugLevel == "show" {
+		fmt.Println("Supported subsystems", supportedSubsystems())
+		os.Exit(0)
+	}
+
+	// Parse, validate, and set debug log level(s).
+	if cfg.Quiet {
+		cfg.ConfigFileOptions.DebugLevel = "error"
+	}
+
+	// Parse, validate, and set debug log level(s).
+	if err := parseAndSetDebugLevels(cfg.DebugLevel); err != nil {
+		err := fmt.Errorf("loadConfig: %s", err.Error())
+		return err
+	}
+
+	if cfg.ConfigFileOptions.VSPInterval < 300 {
+		log.Warn("VSP collection interval cannot be less that 300, setting to 300")
+		cfg.ConfigFileOptions.VSPInterval = 300
+	}
+
+	// check if we can execute the needed op without connecting to a wallet
+	// if len(args) == 0, then there's nothing to execute as all command-line args were parsed as app options
+	if len(args) > 0 {
+		if ok, err := attemptExecuteSimpleOp(); ok {
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+	}
+
+
 	// Display app version.
-	log.Infof("%s version %v (Go version %s)", version.AppName,
-		version.Version(), runtime.Version())
+	log.Infof("%s version %v (Go version %s)", app.AppName,
+		app.Version(), runtime.Version())
 
 	db, err := postgres.NewPgDb(cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPass, cfg.DBName)
 
@@ -249,4 +290,32 @@ func netParams(netType string) *chaincfg.Params {
 	default:
 		return nil
 	}
+}
+
+// attemptExecuteSimpleOp checks if the operation requested by the user does not require a connection to a decred wallet
+// such operations may include cli commands like `help`, ergo a flags parser object is created with cli commands and flags
+// help flag errors (-h, --help) are also handled here, since they do not require access to wallet
+func attemptExecuteSimpleOp() (isSimpleOp bool, err error) {
+	configWithCommands := &config.Config{}
+	parser := flags.NewParser(configWithCommands, flags.HelpFlag|flags.PassDoubleDash)
+
+	// re-parse command-line args to catch help flag or execute any commands passed
+	_, err = parser.Parse()
+	if err != nil {
+		flagErr, ok := err.(*flags.Error)
+		if ok && flagErr.Type == flags.ErrHelp {
+			os.Exit(0)
+		}
+	}
+
+	err = nil
+	isSimpleOp = true
+
+	// if parser.Active != nil {
+		help.PrintCommandHelp(os.Stdout, parser.Name, parser.Active)
+	// } else {
+	// 	help.PrintGeneralHelp(os.Stdout, commands.HelpParser(), commands.Categories())
+	// }
+	
+	return
 }
