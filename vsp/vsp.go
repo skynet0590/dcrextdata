90c6818d841ae6dc418378353eced026067e12a3
Ademu Anthony <ademuanthony@gmail.com>
2019-07-20 13:50:31 +0100

    updated and shortened vsp ticks log

--- vsp/vsp.go
+++ vsp/vsp.go
@@ -79,12 +79,10 @@ func (vsp *Collector) Run(ctx context.Context) {
 		}
 	}
 
-	log.Info("Fetching VSP from source")
-
 	err := vsp.collectAndStore(ctx)
 	app.ReleaseForNewModule()
 	if err != nil {
-		log.Errorf("Could not start collection: %v", err)
+		log.Errorf("Could not start collection: %s", err.Error())
 		return
 	}
 
@@ -104,7 +102,6 @@ func (vsp *Collector) Run(ctx context.Context) {
 						break
 					}
 				}
-				log.Info("Starting a VSP collection cycle")
 				err := vsp.collectAndStore(ctx)
 				app.ReleaseForNewModule()
 				if err != nil {
@@ -119,6 +116,7 @@ func (vsp *Collector) collectAndStore(ctx context.Context) error {
 	if ctx.Err() != nil {
 		return ctx.Err()
 	}
+	log.Info("Fetching VSP from source")
 
 	resp := new(Response)
 	err := vsp.fetch(ctx, resp)
@@ -130,9 +128,7 @@ func (vsp *Collector) collectAndStore(ctx context.Context) error {
 		err = vsp.fetch(ctx, resp)
 	}
 
-	// log.Infof("Collected data for %d vsps", len(*resp))
-
-	errs := vsp.dataStore.StoreVSPs(ctx, *resp)
+	numberStored, errs := vsp.dataStore.StoreVSPs(ctx, *resp)
 	for _, err = range errs {
 		if err != nil {
 			if e, ok := err.(PoolTickTimeExistsError); ok {
@@ -143,5 +139,7 @@ func (vsp *Collector) collectAndStore(ctx context.Context) error {
 			}
 		}
 	}
+
+	log.Infof("Saved ticks for %d VSPs from %s", numberStored, requestURL)
 	return nil
 }
