5e8a225f721c32ea0d9418a68439c35cec87c0d2
edono morrison <edonomorrison@gmail.com>
2019-07-23 10:30:53 +0100

    rebased Pr

--- web/public/app/src/controllers/mempool_controller.js
+++ web/public/app/src/controllers/mempool_controller.js
@@ -10,7 +10,7 @@ export default class extends Controller {
       'nextPageButton', 'previousPageButton', 'tableBody', 'rowTemplate',
       'totalPageCount', 'currentPage', 'btnWrapper', 'tableWrapper', 'chartsView',
       'chartWrapper', 'viewOption', 'labels',
-      'chartDataTypeSelector', 'chartDataType','chartOptions', 'labels', 'selectedMempoolOpt',
+      'chartDataTypeSelector', 'chartDataType', 'chartOptions', 'labels', 'selectedMempoolOpt',
       'selectedNum', 'numPageWrapper'
     ]
   }
