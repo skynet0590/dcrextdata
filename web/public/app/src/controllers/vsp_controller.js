8b2a2f10cfc44ff67575c9de03d5f0ba4cbe757e
Ademu <ademuanthony@gmail.com>
2019-10-19 08:24:03 +0100

    splot vsp chart with cache data

--- web/public/app/src/controllers/vsp_controller.js
+++ web/public/app/src/controllers/vsp_controller.js
@@ -253,8 +253,8 @@ export default class extends Controller {
     showLoading(this.loadingDataTarget, elementsToToggle)
 
     let _this = this
-    const queryString = `data-type=${this.dataType}&vsps=${vsps.join('|')}&view-option=${_this.selectedViewOption}`
-    axios.get(`/vspchartdata?${queryString}`).then(function (response) {
+    const queryString = `axis=${this.dataType}&sources=${vsps.join('|')}`
+    axios.get(`/api/charts/vsp?${queryString}`).then(function (response) {
       let result = response.data
       hideLoading(_this.loadingDataTarget, elementsToToggle)
       if (result.error) {
