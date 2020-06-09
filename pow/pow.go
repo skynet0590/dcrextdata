259e0f52b71d47f50a8233ad0e6b66a1d03ff125
Ademu Anthony <ademuanthony@gmail.com>
2020-06-09 06:47:22 +0100

    replaced outdated luxor api url

--- pow/pow.go
+++ pow/pow.go
@@ -15,7 +15,7 @@ import (
 
 const (
 	Luxor    = "luxor"
-	LuxorUrl = "http://mining.luxor.tech/API/DCR/stats"
+	LuxorUrl = "http://old.luxor.tech/API/DCR/stats"
 
 	F2pool    = "f2pool"
 	F2poolUrl = "https://api.f2pool.com/decred/"
